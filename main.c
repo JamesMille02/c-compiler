d3a864b016ea7a82b84a408ba02a4788914e7a29
JamesMille02 <144379986+JamesMille02@users.noreply.github.com>
2024-08-07 01:13:54 -0400

    gets characters of file

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
 typedef enum {
     SEMI,
@@ -20,15 +22,47 @@ typedef struct {
     TypeSeparator separatorType;
     TypeLiteral literalType;
     int value;
+    char character;
 } Token;
+
+
+
+void lexer(FILE *file){
+    char current_char = fgetc(file);
+    
+    while(current_char != EOF){
+        if(current_char == ';') {
+            printf("found Semicolon \n");
+        } else if (current_char == '(') {
+            printf("found open par \n");
+        } else if (current_char == ')') {
+            printf("found closed par \n");
+        } else if (isalpha(current_char)) {
+            printf("is a character \n");
+        } else if (isdigit(current_char)) {
+            char s1[20] ={0};
+            int index = 0;
+            
+            while(isdigit(current_char) && index < sizeof(s1) -1) {
+                s1[index++] = current_char;
+                current_char = fgetc(file);
+            }
+            s1[index] = '\0';
+            int combined_int = atoi(s1);
+            printf("combined digit: %d\n", combined_int);
+
+            if (!isdigit(current_char)) {
+                fseek(file, -1, SEEK_CUR);
+            }  
+        } else {
+        }
+        current_char = fgetc(file);
+    }
+
+}
 
 int main(){
     FILE *file;
     file = fopen("code.jlang", "r");
-    char current = fgetc(file);
-
-    while(current!= EOF){
-        printf("%c", current);
-        current = fgetc(file);
-    }
+    lexer(file);
 }
