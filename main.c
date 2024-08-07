0b99ca768c8a6e2d47fb8d4e74ad3be11e10133c
JamesMille02 <144379986+JamesMille02@users.noreply.github.com>
2024-08-07 03:56:44 -0400

    mem allocation and error handling

--- main.c
+++ main.c
@@ -10,41 +10,89 @@ typedef enum {
     OPEN_BRACE,
     CLOSED_BRACE,
     SEMICOLON,
-} TypeSeparator;
+    CHARACTER,
+    STRING_LITERAL,
+    INTEGER
+} TokenType;
 
-typedef enum{
+typedef enum {
     EXIT,
     RETURN,
     MAIN,
-    CLASS,
+    CLASS
 } TypeKeyword;
 
-typedef enum{
+typedef enum {
     ADDITION,
     SUBTRACTION,
     MULTIPLICATION,
-    DIVISION,
+    DIVISION
 } TypeOperations;
 
-typedef enum {
-    INT,
-} TypeInteger;
-
-
-typedef struct{
-    TypeSeparator separatorType;
-    TypeInteger integerType;
+typedef struct {
+    TokenType type;
     TypeKeyword keywordType;
     TypeOperations operationType;
     int value;
-    char character;
-    char word[256];
+    char *word;
 } Token;
+
+Token readFullWord(char current_char, FILE *file) {
+    Token token;
+    token.type = CHARACTER;
+    token.word = (char *)malloc(sizeof(char) * 2); 
+    int word_index = 0;
+    int buffer_size = 2;
+
+    token.word[word_index++] = current_char;
+
+    while (isalpha((current_char = fgetc(file))) && current_char != EOF) {
+        if (word_index >= buffer_size - 1) { 
+            buffer_size *= 2;
+            token.word = (char *)realloc(token.word, buffer_size * sizeof(char));
+        }
+        token.word[word_index++] = current_char;
+    }
+    token.word[word_index] = '\0';
+
+    if (current_char != EOF) {
+        fseek(file, -1, SEEK_CUR);
+    }
+
+    return token;
+}
+
+Token readStringLiteral(char current_char, FILE *file) {
+    Token token;
+    token.type = STRING_LITERAL;
+    token.word = (char *)malloc(sizeof(char) * 2); 
+    int index = 0;
+    int buffer_size = 2;
+
+    current_char = fgetc(file);
+
+    while (current_char != '"' && current_char != EOF) {
+        if (index >= buffer_size - 1) { 
+            buffer_size *= 2;
+            token.word = (char *)realloc(token.word, buffer_size * sizeof(char));
+        }
+        token.word[index++] = current_char;
+        current_char = fgetc(file);
+    }
+    token.word[index] = '\0';
+
+    if (current_char != '"') {
+        fprintf(stderr, "Error: Unclosed string literal\n");
+    }
+
+    return token;
+}
 
 Token readFullNumber(char current_char, FILE *file) {
     Token token;
-    token.integerType = INT;
-    int value = current_char - '0';
+    token.type = INTEGER;
+    int value = 0;
+    value = current_char - '0';
     while (isdigit((current_char = fgetc(file))) && current_char != EOF) {
         value = value * 10 + (current_char - '0');
     }
@@ -55,50 +103,40 @@ Token readFullNumber(char current_char, FILE *file) {
     return token;
 }
 
-Token readFullWord(char current_char, FILE *file) {
-    Token token;
-    token.keywordType = CHARACTER;
-    int word_index = 0;
-
-    token.word[word_index++] = current_char;
-
-    while (isalpha((current_char = fgetc(file))) && current_char != EOF) {
-        if(word_index < sizeof(token.word) -1){
-            token.word[word_index++] = current_char;
-        }
-    }
-    token.word[word_index] = '\0';
-    if (current_char != EOF) {
-        fseek(file, -1, SEEK_CUR);
-    }
-    return token;
-}
-
-
-void lexer(FILE *file){
+void lexer(FILE *file) {
     char current_char = fgetc(file);
     
-    while(current_char != EOF){
-        if(current_char == ';') {
-            printf("found Semicolon \n");
+    while (current_char != EOF) {
+        if (current_char == ';') {
+            printf("found Semicolon\n");
         } else if (current_char == '(') {
-            printf("found open par \n");
+            printf("found open par\n");
         } else if (current_char == ')') {
-            printf("found closed par \n");
+            printf("found closed par\n");
         } else if (isalpha(current_char)) {
-            Token token = readFullWord(current_char, file);
-            printf("is a word: %s\n", token.word);
+            Token token_char = readFullWord(current_char, file);
+            printf("is a word: %s\n", token_char.word);
+            free(token_char.word); 
         } else if (isdigit(current_char)) {
-            Token token_int_test = readFullNumber(current_char, file);
-            printf("token value %d\n", token_int_test.value);
+            Token token_int = readFullNumber(current_char, file);
+            printf("token value %d\n", token_int.value);
+        } else if (current_char == '"') {
+            Token token_str = readStringLiteral(current_char, file);
+            printf("string literal: %s\n", token_str.word);
+            free(token_str.word); 
         }
         current_char = fgetc(file);
     }
-
 }
 
-int main(){
+int main() {
     FILE *file;
     file = fopen("code.jlang", "r");
+    if (!file) {
+        fprintf(stderr, "Error opening file\n");
+        return 1;
+    }
     lexer(file);
+    fclose(file);
+    return 0;
 }
