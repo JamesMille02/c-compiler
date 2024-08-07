e43b46c7c2bdfbabb71038ca503ec80f812e5137
JamesMille02 <144379986+JamesMille02@users.noreply.github.com>
2024-08-07 14:34:32 -0400

    created keyword detection

--- main.c
+++ main.c
@@ -9,45 +9,64 @@ typedef enum {
     CLOSED_PARENTHESIS,
     OPEN_BRACE,
     CLOSED_BRACE,
-    SEMICOLON,
     CHARACTER,
     STRING_LITERAL,
+    SINGLE_QUOTE
+} TypeSeparators;
+
+typedef enum {
     INTEGER
-} TokenType;
+} TypeInteger;
 
 typedef enum {
     EXIT,
     RETURN,
     MAIN,
-    CLASS
+    CLASS,
+    IF,
+    ELSE,
+    IFELSE,
+    WHILE,
+    DO,
+    BREAK,
+    UNKNOWN_KEYWORD
 } TypeKeyword;
 
 typedef enum {
     ADDITION,
     SUBTRACTION,
     MULTIPLICATION,
-    DIVISION
+    DIVISION,
+    GREATER_THAN,
+    LESS_THAN,
+    EQUAL_TO
 } TypeOperations;
 
 typedef struct {
-    TokenType type;
+    TypeSeparators separatorType;
     TypeKeyword keywordType;
     TypeOperations operationType;
+    TypeInteger integerType;
     int value;
     char *word;
 } Token;
 
+const char *keywords[] = {
+    "exit", "return", "main", "class", "if", "else", "ifelse", "while", "do", "break"
+};
+#define NUM_KEYWORDS (sizeof(keywords) / sizeof(keywords[0]))
+
 Token readFullWord(char current_char, FILE *file) {
     Token token;
-    token.type = CHARACTER;
-    token.word = (char *)malloc(sizeof(char) * 2); 
+    token.separatorType = CHARACTER;
+    token.word = (char *)malloc(sizeof(char) * 2);
     int word_index = 0;
     int buffer_size = 2;
 
     token.word[word_index++] = current_char;
 
     while (isalpha((current_char = fgetc(file))) && current_char != EOF) {
-        if (word_index >= buffer_size - 1) { 
+        if (word_index >= buffer_size - 1) {
             buffer_size *= 2;
             token.word = (char *)realloc(token.word, buffer_size * sizeof(char));
         }
@@ -59,20 +78,34 @@ Token readFullWord(char current_char, FILE *file) {
         fseek(file, -1, SEEK_CUR);
     }
 
+    token.keywordType = UNKNOWN_KEYWORD;
+    for (int i = 0; i < NUM_KEYWORDS; i++) {
+        if (strcmp(token.word, keywords[i]) == 0) {
+            token.keywordType = (TypeKeyword)i;
+            break;
+        }
+    }
+
+    if (token.keywordType == UNKNOWN_KEYWORD) {
+        fprintf(stderr, "Error: Unknown keyword: '%s'\n", token.word);
+        free(token.word);
+        exit(EXIT_FAILURE);
+    }
+
     return token;
 }
 
-Token readStringLiteral(char current_char, FILE *file) {
+Token readStringLiteral(FILE *file) {
     Token token;
-    token.type = STRING_LITERAL;
-    token.word = (char *)malloc(sizeof(char) * 2); 
+    token.separatorType = STRING_LITERAL;
+    token.word = (char *)malloc(sizeof(char) * 2);
     int index = 0;
     int buffer_size = 2;
 
-    current_char = fgetc(file);
+    char current_char = fgetc(file);
 
     while (current_char != '"' && current_char != EOF) {
-        if (index >= buffer_size - 1) { 
+        if (index >= buffer_size - 1) {
             buffer_size *= 2;
             token.word = (char *)realloc(token.word, buffer_size * sizeof(char));
         }
@@ -90,7 +123,7 @@ Token readStringLiteral(char current_char, FILE *file) {
 
 Token readFullNumber(char current_char, FILE *file) {
     Token token;
-    token.type = INTEGER;
+    token.integerType = INTEGER;
     int value = 0;
     value = current_char - '0';
     while (isdigit((current_char = fgetc(file))) && current_char != EOF) {
@@ -115,13 +148,14 @@ void lexer(FILE *file) {
             printf("found closed par\n");
         } else if (isalpha(current_char)) {
             Token token_char = readFullWord(current_char, file);
-            printf("is a word: %s\n", token_char.word);
+            printf("is a keyword: %s\n", token_char.word);
             free(token_char.word); 
         } else if (isdigit(current_char)) {
             Token token_int = readFullNumber(current_char, file);
             printf("token value %d\n", token_int.value);
         } else if (current_char == '"') {
-            Token token_str = readStringLiteral(current_char, file);
+            Token token_str = readStringLiteral(file);
+            if (token_str != "error")
             printf("string literal: %s\n", token_str.word);
             free(token_str.word); 
         }
